7fc96fc03651f2b4a5b22a8c9d4841524519d98e
Sergey <sergey.potapov.2002@mail.ru>
2022-07-21 22:07:26 +0500

    'remove-counter-of-favicon.ico'

--- server.js
+++ server.js
@@ -3,15 +3,18 @@ const http = require('http');
 let requestsCount = 0; 
 
 const server = http.createServer((request, response) => {
-
+	
 	switch (request.url) {
 		case '/students':
 			response.write('STUDENTS ');
+			break;
 		default:
 			response.write('404 NOT FOUND ');
 	};
 
-	requestsCount++;
+	if (request.url !== '/favicon.ico') {
+		requestsCount++;
+	};
 	response.write('Hello!' + ' Requests count: ' + requestsCount);
 	response.end();
 });
